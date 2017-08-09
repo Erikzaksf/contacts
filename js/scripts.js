d84aff2ad3b5ef299f330b408de7edd8430bff11
Erik Zakrzewski and Mitch Long <erik@>
2017-08-09 15:29:19 -0700

    add formatting to each address entry that is displayed to the page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
-  ("").submit(function(event){
-
-
-
+  $("#formOne").submit(function(event){
     event.preventDefault();
+    var nameInput = $("input#name").val();
+    var addressInput = $("input#address").val();
+    var phoneInput = $("input#phone").val();
+    var relationshipInput = $("input#relationship").val();
+
+    $(".name").text(nameInput);
+    $(".address").text(addressInput);
+    $(".phone").text(addressInput);
+    $(".relationship").text(relationshipInput);
+    $(".contact").append("<br>" +"<br>" + nameInput + "<br>" + addressInput + "<br>" + phoneInput +  "<br>" + relationshipInput);
+
+
   });
 });
